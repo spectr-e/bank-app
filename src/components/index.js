adceb63bdeb286255b1770d5127ae1015fe78532
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 22:47:17 +0300

    feat(Footer): Generate starter code for the component

--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,11 @@ import Button from './Button'
 import CardDeal from './CardDeal'
 import Clients from './Clients'
 import CTA from './CTA'
+import Footer from './Footer'
 import Stats from './Stats'
 import Testimonials from './Testimonials'
 export {
+  Footer,
   CTA,
   Stats,
   Testimonials,
