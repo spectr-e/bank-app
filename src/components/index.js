aecb3993501b2989145d3fed81252dbca0e2d9b2
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 22:48:23 +0300

    feat(FeedbackCard): Generate starter code for the component

--- src/components/index.js
+++ src/components/index.js
@@ -4,10 +4,12 @@ import Button from './Button'
 import CardDeal from './CardDeal'
 import Clients from './Clients'
 import CTA from './CTA'
+import FeedbackCard from './FeedbackCard'
 import Footer from './Footer'
 import Stats from './Stats'
 import Testimonials from './Testimonials'
 export {
+  FeedbackCard,
   Footer,
   CTA,
   Stats,
