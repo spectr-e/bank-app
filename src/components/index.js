ffc0effce8596f7d83649e951c5edcf6b33b3e62
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 22:45:58 +0300

    feat(Stats): Generate starter code for the component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Business from './Business'
 import Button from './Button'
 import CardDeal from './CardDeal'
 import Clients from './Clients'
+import Stats from './Stats'
 import Testimonials from './Testimonials'
-export { Testimonials, CardDeal, Button, Clients, Billing, Business }
+export { Stats, Testimonials, CardDeal, Button, Clients, Billing, Business }
