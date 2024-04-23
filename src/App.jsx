b4d6c8f35f95f0ea1182606f47ebf3e184a4ca0a
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-23 22:33:16 +0300

    fix(App.jsx): Add navbar component structure to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import styles from './styles/style'
+
 const App = () => {
-  return <div className='w-full overflow-hidden bg-primary'>{/* Navbar */}</div>
+  return (
+    <div className='w-full overflow-hidden bg-primary'>
+      {/* Navbar Structure*/}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+    </div>
+  )
 }
 
 export default App
