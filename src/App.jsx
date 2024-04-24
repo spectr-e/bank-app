f5a9c7222d6734782d794486cefd32ceb904122e
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-24 08:40:35 +0300

    fix(assets): Resolve the import tags to match existing files

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,23 @@ const App = () => {
     <div className='w-full overflow-hidden bg-primary'>
       {/* navbar section*/}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
       </div>
 
       {/* hero section */}
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       {/* other sections */}
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+          <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials />
+          <Clients /> <CTA /> <Footer />
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,10 @@
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
+
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <nav className='flex items-center justify-between w-full py-6 navbar'></nav>
+  )
 }
 
 export default NavBar
