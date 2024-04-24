ec1601c68b8cb2f7aa1a7c9dcf6e0ddb8f85f0ad
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-24 10:35:19 +0300

    fix(GetStarted): Structure the inner circle

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -5,7 +5,12 @@ const GetStarted = () => (
   // outer circle
   <div
     className={`${styles.flexCenter} w-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer h-[140px]`}
-  ></div>
+  >
+    {/* inner circle */}
+    <div
+      className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
+    ></div>
+  </div>
 )
 
 export default GetStarted
