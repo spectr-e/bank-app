06c15c6b834b5437f4477bf44adebb12eb0884d8
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-24 08:59:37 +0300

    fix(NavBar): Create an open close icon for mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className='flex items-center justify-between w-full py-6 navbar'>
+      {/* the logo goes here */}
       <img src={logo} alt='hoobank logo' className='w-[124px] h-[32px]' />
+
+      {/* desktop version of the nav links */}
       <ul className='items-center justify-end flex-1 hidden list-none sm:flex'>
         {navLinks.map((link, index) => (
           <li
@@ -17,6 +23,17 @@ const NavBar = () => {
           </li>
         ))}
       </ul>
+
+      {/* mobile version of the nav links */}
+      <div className='flex items-center justify-end flex-1 sm:hidden'>
+        {/* click this icon to expand the menu */}
+        <img
+          src={toggle ? close : menu}
+          alt='open|close menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle(!toggle)}
+        />
+      </div>
     </nav>
   )
 }
