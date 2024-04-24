e618828c7f0dfba837f72ba3c88b9d60c500e7fe
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-24 08:51:23 +0300

    fix(NavBar): Add logo and nav links to component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,21 @@ import { navLinks } from '../constants'
 
 const NavBar = () => {
   return (
-    <nav className='flex items-center justify-between w-full py-6 navbar'></nav>
+    <nav className='flex items-center justify-between w-full py-6 navbar'>
+      <img src={logo} alt='hoobank logo' className='w-[124px] h-[32px]' />
+      <ul className='items-center justify-end flex-1 hidden list-none sm:flex'>
+        {navLinks.map((link, index) => (
+          <li
+            key={index}
+            className={`font-normal cursor-pointer text-white text-[16px] font-poppins  ${
+              index === navLinks.length - 1 ? 'mr-0' : 'mr-10'
+            }`}
+          >
+            <a href={`#${link.id}`}>{link.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
