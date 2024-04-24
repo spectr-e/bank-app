0a504eb65e0ffa41dcbd24934618e5e4970319a1
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-24 10:35:50 +0300

    fix(GetStarted): Add 'Get' + arrowup icon

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -9,7 +9,19 @@ const GetStarted = () => (
     {/* inner circle */}
     <div
       className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}
-    ></div>
+    >
+      {/* text + icon */}
+      <div className={`${styles.flexStart} flex-row`}>
+        <p className='font-poppins font-medium text-[18px] leading-[24px] mr-2'>
+          <span className='text-gradient'>Get</span>
+        </p>
+        <img
+          src={arrowUp}
+          alt='arrowup'
+          className='w-[24px] h-[24px] object-contain'
+        />
+      </div>
+    </div>
   </div>
 )
 
