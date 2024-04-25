8c03272b93d8313cab707332eb1e46895d12f319
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 22:12:35 +0300

    fix(Billing): Add section image gradient to component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -8,8 +8,11 @@ const Billing = () => (
       <img
         src={bill}
         alt='billing'
-        className='w-[100%] h-[100%] relative z-[4]'
+        className='w-[100%] h-[100%] relative z-[5]'
       />
+      {/* gradients */}
+      <div className='absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient' />
+      <div className='absolute z-[0] -left-1/2 bottom-0 w-[50%] h-[50%] rounded-full pink__gradient' />
     </div>
   </section>
 )
