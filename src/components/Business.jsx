feb058fce35f01dab19bbb05e143056e8d225de0
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 21:33:57 +0300

    fix(Business): Add textinfo section title and paragraph

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,5 +1,27 @@
+import { features } from '../constants'
+import styles, { layout } from '../styles/style'
+import Button from './Button'
+
 const Business = () => {
-  return <div>Business</div>
+  return (
+    // entire section
+    <section id='features' className={layout.section}>
+      {/* first column - textInfo */}
+      <div className={layout.sectionInfo}>
+        {/* textInfo title */}
+        <h2 className={`${styles.heading2}`}>
+          You do the business, <br className='hidden sm:block' />
+          we'll handle the money.
+        </h2>
+        {/* textInfo paragraph */}
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default Business
