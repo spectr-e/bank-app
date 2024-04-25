680d5431048325e4159f5b560364007095a5222d
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 21:22:17 +0300

    fix(Stats): Add the individual statistics' labels

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,9 +5,14 @@ const Stats = () => (
   <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
     {stats.map((stat, i) => (
       <div key={i}>
+        {/* numbers */}
         <h4 className='font-poppins font-semibold xs:leading-[52px] xs:text-[40px] text-[32px] leading-[44px] text-white'>
           {stat.value}
         </h4>
+        {/* labels */}
+        <p className='font-poppins font-semibold xs:leading-[24px] xs:text-[20px] leading-[20px] text-white text-[16px] text-gradient ml-3'>
+          {stat.title}
+        </p>
       </div>
     ))}
   </section>
