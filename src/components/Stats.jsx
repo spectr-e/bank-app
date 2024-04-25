f7cb5b04f436201a4e6eb01f8b683522fc2f92b5
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 21:21:43 +0300

    fix(Stats): Add the individual statistics in terms of numbers

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,15 @@
-const Stats = () => {
-  return <div>Stats</div>
-}
+import { stats } from '../constants'
+import styles from '../styles/style'
 
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((stat, i) => (
+      <div key={i}>
+        <h4 className='font-poppins font-semibold xs:leading-[52px] xs:text-[40px] text-[32px] leading-[44px] text-white'>
+          {stat.value}
+        </h4>
+      </div>
+    ))}
+  </section>
+)
 export default Stats
