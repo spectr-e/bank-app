e78891486b4dba636962e3dcf12901834882233b
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 22:25:48 +0300

    fix(CardDeal): Add textInfo column to component with cta button

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,25 @@
+import { card } from '../assets'
+import styles, { layout } from '../styles/style'
+import Button from './Button'
+
 const CardDeal = () => {
-  return <div>CardDeal</div>
+  return (
+    <section id='card' className={layout.section}>
+      {/* textInfo section */}
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className='hidden sm:block' />
+          in a few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
+          mollitia voluptatem recusandae? Cum, itaque blanditiis?
+        </p>
+        {/* getStarted Button */}
+        <Button styles={'mt-10'} />
+      </div>
+    </section>
+  )
 }
 
 export default CardDeal
