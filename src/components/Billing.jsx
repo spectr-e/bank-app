b31b24b35d6010279a4d0689a3296e13f02b26a9
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 22:09:57 +0300

    fix(Billing): Add section structure and image to component

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,5 +1,17 @@
-const Billing = () => {
-  return <div>Billing</div>
-}
+import { apple, bill, google } from '../assets'
+import styles, { layout } from '../styles/style'
+
+const Billing = () => (
+  <section id='product' className={layout.sectionReverse}>
+    {/* section image */}
+    <div className={layout.sectionImgReverse}>
+      <img
+        src={bill}
+        alt='billing'
+        className='w-[100%] h-[100%] relative z-[4]'
+      />
+    </div>
+  </section>
+)
 
 export default Billing
