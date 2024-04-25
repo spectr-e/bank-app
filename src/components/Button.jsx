8ff6ab17255e415c3e78c8e031a1b79861d6b48a
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 22:34:59 +0300

    fix(Button): Add font semibold to the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ styles }) => {
   return (
     <button
       type='button'
-      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[16px] text-primary outline-none rounded-lg ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-semibold text-[16px] text-primary outline-none rounded-lg ${styles}`}
     >
       Get Started
     </button>
