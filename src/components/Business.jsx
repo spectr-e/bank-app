e3d24473082f06f8e43e2a231dcef0c0ef0f2393
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-25 21:38:51 +0300

    fix(Button): Add the get started button to the textinfo section of the business area

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -19,6 +19,9 @@ const Business = () => {
           building credit, earning rewards and saving money. But with hundreds
           of credit cards on the market.
         </p>
+
+        {/* get started button */}
+        <Button styles={'mt-10'} />
       </div>
     </section>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,12 @@
-const Button = () => {
-  return <div>Button</div>
+const Button = ({ styles }) => {
+  return (
+    <button
+      type='button'
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[16px] text-primary outline-none rounded ${styles}`}
+    >
+      Get Started
+    </button>
+  )
 }
 
 export default Button
