7c6b2c575ea122817931756d3d0e1971e764f039
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:14:07 +0300

    fix(FeedbackCard): Add double quotes img

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,5 +1,15 @@
-const FeedbackCard = () => {
-  return <div>FeedbackCard</div>
-}
+import { quotes } from '../assets'
+
+const FeedbackCard = ({ content, title, name, img }) => (
+  <div
+    className={`flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] mr-0 my-5 feedback-card md:mr-10 sm:mr-5`}
+  >
+    <img
+      src={quotes}
+      alt='double_quotes'
+      className='w-[40px] h-[28px] object-contain'
+    />
+  </div>
+)
 
 export default FeedbackCard
