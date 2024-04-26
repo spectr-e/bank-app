c6d74afc00c02fb95130eb565e7728e1e6cfa295
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:54:28 +0300

    fix(CTA): Add structure to the section component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,5 +1,10 @@
-const CTA = () => {
-  return <div>CTA</div>
-}
+import styles from '../styles/style.js'
+import Button from './Button'
+
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col sm:flex-row rounded-[20px] box-shadow bg-black-gradient-2`}
+  ></section>
+)
 
 export default CTA
