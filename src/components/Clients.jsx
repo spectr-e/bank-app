1c9495933a7c5800fbea12ba0061d7f108cf9e83
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:41:43 +0300

    fix(Clients): Map over the clients info and display their logo

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -3,7 +3,20 @@ import { clients } from '../constants'
 
 const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
-    <div className={`${styles.flexCenter} w-full flex-wrap`}></div>
+    <div className={`${styles.flexCenter} w-full flex-wrap`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[196px] min-w-[120px] `}
+        >
+          <img
+            src={client.logo}
+            alt='client'
+            className='sm:w-[196px] w-[100px] '
+          />
+        </div>
+      ))}
+    </div>
   </section>
 )
 
