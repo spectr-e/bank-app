2af5c1e4b2c05a06898da7fd48f4d1108007ca13
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 11:28:07 +0300

    fix(Footer): Add footer link titles to second column

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,6 +17,25 @@ const Footer = () => (
           A new way to make payments easy, reliable and secure.
         </p>
       </div>
+
+      {/* 2nd column - links */}
+      <div
+        className={`flex-[1.5] w-full flex-row flex-wrap flex justify-between mt-10 md:mt-10`}
+      >
+        {footerLinks.map((footerLink, i) => (
+          <div
+            className={`flex flex-col my-4 min-w-[150px] ss:my-0`}
+            key={footerLink.key}
+          >
+            {/* titles */}
+            <h4
+              className={`font-poppins font-medium text-[18px] leading-[28px] text-white`}
+            >
+              {footerLink.title}
+            </h4>
+          </div>
+        ))}
+      </div>
     </div>
   </section>
 )
