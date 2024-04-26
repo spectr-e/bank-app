fa9468a7c9871c9fb78e09187facfa4726ecc273
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:57:31 +0300

    fix(Footer): Import styles, logo, links and socials to component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
+import styles from '../styles/style.js'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants/'
+
 const Footer = () => {
   return <div>Footer</div>
 }
