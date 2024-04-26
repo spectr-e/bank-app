0e00577465c9306d8500cf2e09071ef29390c47d
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:55:09 +0300

    fix(CTA): Add title + content of the section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,16 @@ import Button from './Button'
 const CTA = () => (
   <section
     className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} flex-col sm:flex-row rounded-[20px] box-shadow bg-black-gradient-2`}
-  ></section>
+  >
+    {/* title + content */}
+    <div className={`flex-1 flex flex-col`}>
+      <h2 className={styles.heading2}>Try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+  </section>
 )
 
 export default CTA
