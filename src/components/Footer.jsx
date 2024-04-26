5b66c4fedee9716959805686ca81a472d88174de
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 11:26:46 +0300

    fix(Footer): Add bank logo and catch phrase to first column

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,22 @@ import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants/'
 
 const Footer = () => (
-  <section
-    className={`${styles.flexCenter} ${styles.paddingY} flex-col`}
-  ></section>
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    {/* links section */}
+    <div className={`${styles.flexStart} flex-col md:flex-row mb-8 w-full`}>
+      {/* 1st column - logo */}
+      <div className={`flex-1 flex flex-col justify-start m-10`}>
+        <img
+          src={logo}
+          alt='logo'
+          className='w-[266px] h-[72px] object-contain'
+        />
+        <p className={`mt-4 max-w-[310px] ${styles.paragraph}`}>
+          A new way to make payments easy, reliable and secure.
+        </p>
+      </div>
+    </div>
+  </section>
 )
 
 export default Footer
