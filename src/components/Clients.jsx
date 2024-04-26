3776b0b5f8d8552cee9088c67cc2c3380895873b
spectr-e <81918288+spectr-e@users.noreply.github.com>
2024-04-26 10:41:03 +0300

    fix(Clients): Add structure to the component

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,10 @@
-const Clients = () => {
-  return <div>Clients</div>
-}
+import styles from '../styles/style.js'
+import { clients } from '../constants'
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} w-full flex-wrap`}></div>
+  </section>
+)
 
 export default Clients
